8b4b30b12d2ecc259fa353f1081831c9ed1f08cb
Sotirios Pupakis <sotirek@gmail.com>
2023-11-08 14:22:41 +0100

    Added tables

---
+++ addons/mortars_main/tables/config.cpp
@@ -0,0 +1,13 @@
+#include "..\script_component.hpp"
+
+class CfgPatches {
+    class tbd_mortars_tables {
+        addonRootClass = "A3_Armor_F_Beta";
+        requiredAddons[] = {"cba_main", "ace_interact_menu", "rhsusf_c_statics"};
+        requiredVersion = 0.1;
+        weapons[] = {""};
+        units[] = {};
+    };
+};
+
+#include "CfgWeapons.hpp"
