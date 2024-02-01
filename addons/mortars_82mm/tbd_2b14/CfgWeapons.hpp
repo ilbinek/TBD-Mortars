be1cc62628c2ff5a7084be505c0d22dbba152516
Sotirios Pupakis <sotirek@gmail.com>
2024-02-01 11:42:59 +0100

    WOG release

--- addons/mortars_82mm/tbd_2b14/CfgWeapons.hpp
+++
@@ -1,7 +0,0 @@
-class CfgWeapons {
-    class mortar_82mm;
-    class rhs_weap_2b14: mortar_82mm {
-        magazineReloadTime = 0.2;
-		magazines[] += {QUOTE(TBD_MORTAR_82mm_HE), QUOTE(TBD_MORTAR_82mm_SMOKE), QUOTE(TBD_MORTAR_82mm_ILLUM)};
-    };
-};
--- addons/mortars_82mm/tbd_mk6/CfgWeapons.hpp
+++
@@ -1,7 +0,0 @@
-class CfgWeapons {
-    class CannonCore;
-    class mortar_82mm: CannonCore {
-        magazineReloadTime = 0.2;
-		magazines[] += {QUOTE(TBD_MORTAR_82mm_HE), QUOTE(TBD_MORTAR_82mm_SMOKE), QUOTE(TBD_MORTAR_82mm_ILLUM)};
-    };
-};
