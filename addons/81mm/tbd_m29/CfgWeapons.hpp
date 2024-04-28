7624c51b3d264820b43ebb0dc4e9cc625977c24c
Sotirios Pupakis <sotirek@gmail.com>
2024-04-28 16:31:50 +0200

    removed unneeded parts

--- addons/81mm/tbd_m29/CfgWeapons.hpp
+++
@@ -1,7 +0,0 @@
-class CfgWeapons {
-    class mortar_82mm;
-    class vn_mortar_m29: mortar_82mm {
-        magazineReloadTime = 0.2;
-		magazines[] += {QUOTE(TBD_MORTAR_81mm_HE), QUOTE(TBD_MORTAR_81mm_SMOKE), QUOTE(TBD_MORTAR_81mm_ILLUM)};
-    };
-};
