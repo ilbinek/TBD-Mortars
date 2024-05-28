4d1e47539b26dd405cfdf870bf28b86444662e0a
Sotirios Pupakis <sotirek@gmail.com>
2024-05-28 02:42:09 +0200

    ATGM fix

--- addons/100mm/tbd_mortars_100mm_round_atgm/CfgAmmo.hpp
+++ addons/100mm/tbd_mortars_100mm_round_atgm/CfgAmmo.hpp
@@ -1,11 +1,19 @@
 class CfgAmmo {
-	class ace_hot_HOT2;
+	class ace_hot_HOT1;
+	class ace_hot_HOT2: ace_hot_HOT1 {
+		class ace_missileguidance;
+	};
 
 	class TBD_MORTARS_100MM_ROUND_AMMO_ATGM: ace_hot_HOT2 {
-		model = QPATHTOF(TBD_MORTARS_100MM_ROUND_ATGM\TBD_MORTARS_100MM_ROUND_ATGM);
-		hit = 90;
-		indirectHit = 25;
-		indirectHitRange = 3.5;
-		initSpeed = 350;
+		model = QPATHTOF(TBD_MORTARS_100MM_ROUND_ATGM\tbd_mortars_100mm_ammo_atgm);
+		hit = 200;
+		indirectHit = 20;
+		indirectHitRange = 3.6;
+		caliber = 23;
+		typicalSpeed = 975;
+		airFriction = -0.0001;
+		class ace_missileguidance: ace_missileguidance {
+          enabled = 1;
+        };
 	};
 };
