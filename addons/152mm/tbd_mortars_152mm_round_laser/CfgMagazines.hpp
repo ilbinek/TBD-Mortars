f2792c0ae9a50f6ebb6fade509864e6dc902f0e8
ilbinek <sotirios.pupakis@gmail.com>
2024-09-02 20:24:13 +0200

    Removed unused parts

--- addons/152mm/tbd_mortars_152mm_round_laser/CfgMagazines.hpp
+++ addons/152mm/tbd_mortars_152mm_round_laser/CfgMagazines.hpp
@@ -17,21 +17,4 @@ class CfgMagazines {
 		type = 256;
 		initSpeed = 660;
 	};
-	
-	class TBD_MORTARS_152MM_MAG_LASER: 8Rnd_120mm_HE_shells {
-		scope = 2;
-		scopeArsenal = 2;
-		scopeCurator = 2;
-		author = "TBD Team";
-        displayName = CSTRING(ROUND_LASER);
-		displayNameShort = CSTRING(ROUND_LASER);
-        model = QPATHTOF(TBD_MORTARS_152MM_ROUND_LASER\TBD_MORTARS_152MM_ROUND_LASER);
-		picture = QPATHTOF(TBD_MORTARS_152MM_ROUND_LASER\data\laser.paa);
-		descriptionShort = CSTRING(ROUND_LASER);
-        mass = 120;
-		count = 1;
-		ammo = QUOTE(TBD_MORTARS_152MM_ROUND_AMMO_LASER);
-		type = 256;
-		initSpeed = 660;
-	};
 };
--- addons/152mm/tbd_mortars_152mm_round_nuclear/CfgMagazines.hpp
+++ addons/152mm/tbd_mortars_152mm_round_nuclear/CfgMagazines.hpp
@@ -17,21 +17,4 @@ class CfgMagazines {
 		type = 256;
 		initSpeed = 660;
 	};
-	
-	class TBD_MORTARS_152MM_MAG_NUCLEAR: 8Rnd_120mm_HE_shells {
-		scope = 2;
-		scopeArsenal = 2;
-		scopeCurator = 2;
-		author = "TBD Team";
-        displayName = CSTRING(ROUND_NUCLEAR);
-		displayNameShort = CSTRING(ROUND_NUCLEAR);
-        model = QPATHTOF(TBD_MORTARS_152MM_ROUND_NUCLEAR\TBD_MORTARS_152MM_ROUND_NUCLEAR);
-		picture = QPATHTOF(TBD_MORTARS_152MM_ROUND_NUCLEAR\data\nuclear.paa);
-		descriptionShort = CSTRING(ROUND_NUCLEAR);
-        mass = 120;
-		count = 1;
-		ammo = QUOTE(TBD_MORTARS_152MM_ROUND_AMMO_NUCLEAR);
-		type = 256;
-		initSpeed = 660;
-	};
 };
