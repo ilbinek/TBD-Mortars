c25b6f42d48e73efe237d294797d6cc2e1cce675
ilbinek <sotirios.pupakis@gmail.com>
2024-10-20 22:40:09 +0200

    changed spent casing to be vehicle

--- addons/152mm/tbd_mortars_152mm_casing_spent/CfgVehicles.hpp
+++ addons/152mm/tbd_mortars_152mm_casing_spent/CfgVehicles.hpp
@@ -1,18 +1,19 @@
-class CfgMagazines {
-	class 8Rnd_120mm_HE_shells;
+class CfgVehicles {
+	class NATO_Box_Base;
 	
-	class TBD_MORTARS_152MM_CASING_SPENT: 8Rnd_120mm_HE_shells {
+	class TBD_MORTARS_152MM_CASING_SPENT: NATO_Box_Base {
 		scope = 2;
-		scopeArsenal = 2;
-		scopeCurator = 2;
 		author = "TBD Team";
         displayName = CSTRING(CASING_SPENT);
 		displayNameShort = CSTRING(CASING_SPENT);
         model = QPATHTOF(TBD_MORTARS_152MM_CASING_SPENT\TBD_MORTARS_152MM_CASING_SPENT);
 		picture = QPATHTOF(TBD_MORTARS_152MM_CASING_SPENT\data\casing.paa);
 		descriptionShort = CSTRING(CASING_SPENT);
-        mass = 40;
-		type = 256;
-		count = 0;
+
+		ace_cargo_size = 0.5;
+        ace_cargo_canLoad = 1;
+        transportMaxWeapons = 0;
+        transportMaxMagazines = 0;
+        transportMaxBackpacks = 0;
 	};
 };
--- addons/152mm/tbd_mortars_152mm_casing_spent/config.cpp
+++ addons/152mm/tbd_mortars_152mm_casing_spent/config.cpp
@@ -10,4 +10,4 @@ class CfgPatches {
     };
 };
 
-#include "CfgMagazines.hpp"
+#include "CfgVehicles.hpp"
