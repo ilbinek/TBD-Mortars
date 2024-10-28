1c1fbdfb99b405dffb26a95d261e7f2607792fa6
ilbinek <sotirios.pupakis@gmail.com>
2024-10-28 19:54:02 +0100

    minor edits

--- addons/152mm/tbd_mortars_152mm_round_dpicm/CfgAmmo.hpp
+++ addons/152mm/tbd_mortars_152mm_round_dpicm/CfgAmmo.hpp
@@ -1,13 +1,31 @@
 class CfgAmmo {
-	class Sh_120mm_HE;
+	class SubmunitionBase;
+	class Cluster_155mm_AMOS: SubmunitionBase {};
+	class ShellBase;
+	class Mo_cluster_AP: ShellBase {};
 
-	class TBD_MORTARS_152MM_ROUND_AMMO_DPICM: Sh_120mm_HE {
+	class TBD_MORTARS_152MM_ROUND_AMMO_DPICM: Cluster_155mm_AMOS {
 		model = QPATHTOF(TBD_MORTARS_152MM_ROUND_DPICM\TBD_MORTARS_152MM_ROUND_DPICM);
+		submunitionAmmo = QUOTE(TBD_MORTARS_105mm_SHELL_AMMO_DPICM_SUB);
 		hit = 270;
 		indirectHit = 130;
 		indirectHitRange = 35;
 		typicalSpeed = 660;
 		airFriction = 0;
-		timeToLive = 25;
+		triggerOnImpact = 1;
+		triggerDistance = -1;
+	};
+
+	class TBD_MORTARS_152MM_ROUND_AMMO_DPICM_SUB: Mo_cluster_AP {
+		hit = 15;
+		indirectHit = 10;
+		indirectHitRange = 10;
+		ace_frag_skip = 0;
+		ace_frag_metal = 248;
+		ace_frag_charge = 44;
+		ace_frag_gurney_c = 2830;
+		ace_frag_gurney_k = "3/5";
+		ace_frag_fragCount = 1200;
+		ace_frag_classes[] = {"ace_frag_tiny","ace_frag_tiny", "ace_frag_tiny_HD"};
 	};
 };
