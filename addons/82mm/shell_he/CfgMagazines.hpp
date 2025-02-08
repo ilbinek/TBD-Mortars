494aa7f96f4ce3fae223b71c1c99802f4d85bd30
Sotirios Pupakis <sotirek@gmail.com>
2025-02-08 10:11:06 +0100

    example

---
+++ addons/82mm/shell_he/CfgMagazines.hpp
@@ -0,0 +1,55 @@
+class CfgMagazines {
+	class Default;
+	class CA_Magazine: Default {};
+	class VehicleMagazine: CA_Magazine {};
+	class 32Rnd_155mm_Mo_shells: VehicleMagazine {};
+	class 8Rnd_82mm_Mo_shells: 32Rnd_155mm_Mo_shells {};
+
+	class TBD_MORTAR_82mm_HE_CHARGE_1: 8Rnd_82mm_Mo_shells {
+		scope = 2;
+		scopeArsenal = 2;
+		author = "TBD Team";
+		displayName = QUOTE($STR_TBD_MORTAR_82MM_HE_SHELL_CHARGE1);
+		displayNameShort = CSTRING(HE_SHORT);
+		model = QPATHTOF(shell_he\tbd_mortar_82mm_shell_he_charge1);
+		picture = QPATHTOF(shell_he\data\icon.paa);
+		descriptionShort = QUOTE($STR_TBD_MORTAR_82MM_HE_SHELL_DESC);
+		initSpeed = 70;
+		mass = 70;
+		count = 1;
+		ammo = QUOTE(TBD_MORTAR_82mm_SHELL_AMMO_HE);
+		type = 256;
+	};  
+
+	class TBD_MORTAR_82mm_HE_CHARGE_2: 8Rnd_82mm_Mo_shells {
+		scope = 2;
+		scopeArsenal = 2;
+		author = "TBD Team";
+		displayName = QUOTE($STR_TBD_MORTAR_82MM_HE_SHELL_CHARGE2);
+		displayNameShort = CSTRING(HE_SHORT);
+		model = QPATHTOF(shell_he\tbd_mortar_82mm_shell_he_charge2);
+		picture = QPATHTOF(shell_he\data\icon.paa);
+		descriptionShort = QUOTE($STR_TBD_MORTAR_82MM_HE_SHELL_DESC);
+		initSpeed = 140;
+		mass = 70;
+		count = 1;
+		ammo = QUOTE(TBD_MORTAR_82mm_SHELL_AMMO_HE);
+		type = 256;
+	}; 
+
+	class TBD_MORTAR_82mm_HE_CHARGE_3: 8Rnd_82mm_Mo_shells {
+		scope = 2;
+		scopeArsenal = 2;
+		author = "TBD Team";
+		displayName = QUOTE($STR_TBD_MORTAR_82MM_HE_SHELL_CHARGE3);
+		displayNameShort = CSTRING(HE_SHORT);
+		model = QPATHTOF(shell_he\tbd_mortar_82mm_shell_he_charge3);
+		picture = QPATHTOF(shell_he\data\icon.paa);
+		descriptionShort = QUOTE($STR_TBD_MORTAR_82MM_HE_SHELL_DESC);
+		initSpeed = 211;
+		mass = 70;
+		count = 1;
+		ammo = QUOTE(TBD_MORTAR_82mm_SHELL_AMMO_HE);
+		type = 256;
+	}; 
+};																																						
